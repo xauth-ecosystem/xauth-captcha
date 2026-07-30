e560a8154948f2ddbecd07826a2ce44c2ffc13e1
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-30 21:44:34 +0300

    feat: Create CaptchaConfig struct

---
+++ src/config.rs
@@ -0,0 +1,34 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct CaptchaConfig {
+    #[serde(default = "default_width")]
+    pub width: u32,
+    #[serde(default = "default_height")]
+    pub height: u32,
+    #[serde(default = "default_length")]
+    pub length: usize,
+    #[serde(default)]
+    pub noise_dots: Option<usize>,
+    #[serde(default)]
+    pub noise_lines: Option<usize>,
+    #[serde(default)]
+    pub enable_wave: bool,
+}
+
+impl Default for CaptchaConfig {
+    fn default() -> Self {
+        Self {
+            width: default_width(),
+            height: default_height(),
+            length: default_length(),
+            noise_dots: None,
+            noise_lines: None,
+            enable_wave: false,
+        }
+    }
+}
+
+fn default_width() -> u32 { 128 }
+fn default_height() -> u32 { 128 }
+fn default_length() -> usize { 5 }
