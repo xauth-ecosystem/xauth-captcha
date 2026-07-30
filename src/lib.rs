f8e7e77655c42cd25002fb3ea800faf9f06849e5
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-30 21:45:04 +0300

    feat: Expose CaptchaConfig module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod builder;
+pub mod config;
 pub mod filters;
 pub mod font;
 pub mod generator;
 
 pub use builder::CaptchaBuilder;
+pub use config::CaptchaConfig;
 pub use filters::{geometry::GeometryFilter, noise::NoiseFilter, wave::WaveFilter, CaptchaFilter};
